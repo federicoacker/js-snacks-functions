092152fbc909c3f06bd60c8b5f63b96ba779ef8a
federicoacker <acker.federico@gmail.com>
2026-04-01 14:11:33 +0200

    Aggiunto un controllo extra e fatta la logica che crea il nuovo array da restituire per snack3

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -4,17 +4,34 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-function filterByFirstLetter(stringArray, controlString){ // Funzione filterByFirstLetter
-    // Voglio innanzitutto controllare che stringArray sia un array e che controlString sia una stringa, se non lo è farò il toString di controlString o restituirò -1 per l'array.
+function filterByFirstLetter(stringArray, controlChar){ // Funzione filterByFirstLetter
+    // Voglio innanzitutto controllare che stringArray sia un array e che controlChar sia una stringa, se non lo è farò il toString di controlString o restituirò -1 per l'array.
     if(!Array.isArray(stringArray)){
         return -1;
     }
-    if(typeof(controlString) !== "string"){
-        controlString = controlString.toString(); // Questo non dovrebbe causare problemi, perché se mi viene passata 
+    if(typeof(controlChar) !== "string"){
+        controlChar = controlChar.toString(); // Questo non dovrebbe causare problemi, perché se mi viene passata 
         // un tipo semplice, non vado a toccare la variabile che mi è stata passata come argomento perché tanto c'è stata la riassegnazione
-        // un tipo complesso, beh, controlString sta semplicemente cambiando dall'essere assegnata al riferimento dell'indirizzo di memoria
+        // un tipo complesso, beh, controlChar sta semplicemente cambiando dall'essere assegnata al riferimento dell'indirizzo di memoria
         // all'avere un tipo semplice come valore, quindi non vado a modificare nulla della variabile iniziale.
-    }   
+        
+        // Aggiungo un controllo extra che si assicuri che controlChar abbia length esclusivamente di 1
+        if(controlChar.length !== 1){ 
+            return -2; //Se è diverso da 1, ritorno -2
+        }
+    }
+    
+    const filteredArray = []; //Array dove conterremo le parole filtrate secondo la controlChar
+
+    for(let i = 0; i < stringArray.length; i++){
+        let currentString = stringArray[i];
+        if(currentString[0] === controlChar){ //Qui in realtà avremmo potuto usare "startsWith" che avrebbe concesso di controllare 
+        // anche se la controlChar era una stringa di più valori. Ma io voglio che funzioni solo con char semplici.
+            filteredArray.push(currentString); // Lo metto nell'array nuovo
+        }
+    }
+
+    return filteredArray; //Ritorno l'array nuovo
 }
 
 // Invoca la funzione qui e stampa il risultato in console
