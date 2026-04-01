39f44661dba38fc42f9574397fb35192aec59051
federicoacker <acker.federico@gmail.com>
2026-04-01 15:46:16 +0200

    Aggiustato un errore nel primo esercizio dovuto a totale mancanza di comprensione linguistica

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -5,16 +5,13 @@ const userName = 'Mario';
 
 // Dichiara la funzione qui.
 function saluto(name){ //Funzione saluto
-    if(typeof(name) !== "string" || name === ''){ //Controlla se ciò che gli è stato passato è effettivamente una stringa, restituisce errore se non lo è
-        console.error("E' stato passato un valore che non è una stringa, o una stringa vuota alla funzione saluto()");
-    }
-    else{ // Altrimenti restituisce un log con Ciao name
-        console.log(`Ciao ${name}`);
-    }
+    return (typeof(name) !== "string" || name === '') 
+    ? "E' stato passato un valore che non è una stringa, o una stringa vuota alla funzione saluto()" 
+    : `Ciao ${name}`;
 }
 
 // Invoca la funzione qui e stampa il risultato in console
-saluto(userName);
+console.log(saluto(userName));
 
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
