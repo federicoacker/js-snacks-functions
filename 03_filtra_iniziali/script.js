cbbd5be8e8da9bc0e327b35ac97ac1da6f32679e
federicoacker <acker.federico@gmail.com>
2026-04-01 14:06:41 +0200

    Aggiunta prima logica di controllo per lo snack3

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -4,7 +4,18 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-
+function filterByFirstLetter(stringArray, controlString){ // Funzione filterByFirstLetter
+    // Voglio innanzitutto controllare che stringArray sia un array e che controlString sia una stringa, se non lo è farò il toString di controlString o restituirò -1 per l'array.
+    if(!Array.isArray(stringArray)){
+        return -1;
+    }
+    if(typeof(controlString) !== "string"){
+        controlString = controlString.toString(); // Questo non dovrebbe causare problemi, perché se mi viene passata 
+        // un tipo semplice, non vado a toccare la variabile che mi è stata passata come argomento perché tanto c'è stata la riassegnazione
+        // un tipo complesso, beh, controlString sta semplicemente cambiando dall'essere assegnata al riferimento dell'indirizzo di memoria
+        // all'avere un tipo semplice come valore, quindi non vado a modificare nulla della variabile iniziale.
+    }   
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
