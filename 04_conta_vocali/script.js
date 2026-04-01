447263b99a34a907f7dfb3891e96b08b4e775dab
federicoacker <acker.federico@gmail.com>
2026-04-01 14:26:39 +0200

    Aggiunto il conteggio delle vocali alla funzione howManyVocals e il ritorno del valore

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -11,6 +11,14 @@ function howManyVocals(stringToCount){//Funzione che conta il numero di vocali i
     const vocals = ['a','e','i','o','u']; // Array di vocali
     let sumVocals = 0; //Somma di quante vocali ci sono nella parola
 
+    for(let i = 0; i<stringToCount.length; i++){ // Iteriamo nella stringa
+        const currentLetter = stringToCount[i]; // Assegnamo currentLetter alla lettera corrente
+        if(vocals.includes(currentLetter.toLowerCase())){ //Vediamo se vocals include la currentLetter (in lowercase così siamo sicuri che sia A sia a vengano contati);
+            sumVocals++; //Incrememntiamo sumVocals;
+        }
+    }
+
+    return sumVocals; //Restituiamo sumVocals
 }
 
 
