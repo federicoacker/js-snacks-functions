e7f7301a4607c9d7764306b60c6028bee8c59542
federicoacker <acker.federico@gmail.com>
2026-04-01 13:54:22 +0200

    Aggiunto primo controllo a ciò che viene passato alla funzione dello snack2

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -4,6 +4,14 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+function initials(arrayDiStringhe){ //Funzione initials
+    //Vorrei prima controllare che sia effettivamente un array, ritornando -1 se non lo è
+    if(!Array.isArray(arrayDiStringhe)){
+        return -1;
+    }
+    //Vorrei poi fare un controllo per vedere che i valori all'interno siano tutti di tipo string, rimuovendo quelli che non sono stringhe
+
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
